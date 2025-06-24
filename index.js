34782dd415a98c76ab9cf44230f0546eac516cb7
co-den <iamagugbueikenna@gmail.com>
2025-06-24 22:13:37 +0100

    second commit

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ const userRoutes = require("./routes/userRoutes"); // Import user routes
 const app = express();
 connectDB(); // Connect to DB
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chef-assistant-1.onrender.com"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/recipes", recipeRoutes);
